a28ca457bedcab22a1a3ba94c84b87bcb09c44f3
MaulanaAkbar12 <udinu2880@gmail.com>
2025-05-16 16:57:28 +0700

    penyesuaian

--- routes/departemen.js
+++ routes/departemen.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/departemenController");
+const { verifyToken } = require("../middleware/authentication");
 
 /**
  * @swagger
@@ -21,5 +22,76 @@ const controller = require("../controllers/departemenController");
  *         description: Daftar departemen berhasil diambil
  */
 router.get("/", controller.getAllDepartemen);
+
+/**
+ * @swagger
+ * /departemen:
+ *   post:
+ *     summary: Tambah data departemen baru
+ *     tags: [departemen]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nama_departemen:
+ *                 type: string
+ *               deskripsi_departemen:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Departemen berhasil ditambahkan
+ */
+router.post("/", verifyToken, controller.addDepartemen);
+
+/**
+ * @swagger
+ * /departemen/{id}:
+ *   put:
+ *     summary: Update data departemen berdasarkan ID
+ *     tags: [departemen]
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: ID departemen yang akan diperbarui
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nama_departemen:
+ *                 type: string
+ *               deskripsi_departemen:
+ *                 type: string
+ */
+router.put("/:id", verifyToken, controller.updateDepartemen);
+
+/**
+ * @swagger
+ * /departemen/{id}:
+ *   delete:
+ *     summary: Hapus data departemen berdasarkan ID
+ *     tags: [departemen]
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: ID departemen yang akan dihapus
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Departemen berhasil dihapus
+ */
+router.delete("/:id", verifyToken, controller.deleteDepartemen);
+
+
 
 module.exports = router;
